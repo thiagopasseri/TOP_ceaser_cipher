4aaa6a9c7ab1a6ef9c0374c36656a576ea5a9d09
thiago passeri <119812332+thiagopasseri@users.noreply.github.com>
2025-01-14 20:25:13 -0300

    add gemfiles and rubocop files

--- chiper.rb
+++ chiper.rb
@@ -1,9 +1,9 @@
-
 # word = "QEB NRFZH YOLTK CLU GRJMP LSBO QEB IXWV ALD"
-puts "This is the Ceaser Cipher Decoder"
-puts "Type a KEY:"
+
+puts 'This is the Ceaser Cipher Decoder'
+puts 'Type a KEY:'
 key = gets.chomp.to_i
-puts "Type the phrase:"
+puts 'Type the phrase:'
 word = gets.chomp
 
 def decode_cipher(word, key)
@@ -16,14 +16,14 @@ end
 
 def chiper_shift(char, key)
   char_ascii = char.ord
-  if char_ascii.between?(97,122)
-    (((char_ascii - 97) + key)%(122 - 97 + 1) + 97).chr
-  elsif char_ascii.between?(65,90)
-    (((char_ascii - 65) + key)%(90 - 65 + 1) + 65).chr
+  if char_ascii.between?(97, 122)
+    ((((char_ascii - 97) + key) % 26) + 97).chr
+  elsif char_ascii.between?(65, 90)
+    ((((char_ascii - 65) + key) % 26) + 65).chr
   else
     char
   end
 end
 
 puts "original phrase: KEY:#{key}"
-puts decode_cipher(word,key)
+puts decode_cipher(word, key)
